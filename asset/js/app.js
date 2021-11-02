9d7a70c4d5a12900c7ea762826dd29a6026835e3
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-02 16:34:16 +0100

    If Added Task is in either Erased or Completed list it will be removed from that list

--- asset/js/app.js
+++ asset/js/app.js
@@ -26,13 +26,26 @@ const app = new Vue(
             },
             addNewTask() {
                 if (this.newTask != "" && this.newTask.length > 5) {
+
                     this.tasks.push(this.newTask);
+                    this.searchUniqueEntry(this.newTask);
                     this.newTask = "";
                     this.errorCheck = true;
+
                 } else {
                     this.errorCheck = false;
-
                 }
+            },
+            searchUniqueEntry(entry) {
+                if (this.completedTasks.includes(entry)) {
+                    const copyAt = this.completedTask.indexOf(entry);
+                    this.completedTask.splice(copyAt, 1)
+                }
+                if (this.erasedTasks.includes(entry)) {
+                    const copyAt = this.erasedTasks.indexOf(entry);
+                    this.erasedTasks.splice(copyAt, 1)
+                }
+
             }
         }
     })
